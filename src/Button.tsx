2d707602f6457c1b89e4bd84d78508d230a50da3
Aneeshpissay330 <126843748+Aneeshpissay330@users.noreply.github.com>
2025-08-20 12:42:29 +0530

    UI changes

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: React.FC<ButtonProps> = ({ children, ...props }) => (
-  <button {...props} className="px-4 py-2 rounded-lg" >
+  <button {...props} className="px-4 py-2 rounded-lg">
     {children}
   </button>
 );
